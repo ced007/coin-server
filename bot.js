733bb915b3731b556c9fb9abaccea65e18c6327b
ced007 <99514468+ced007@users.noreply.github.com>
2024-11-12 01:42:26 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
     bot.start((ctx) => {
-        ctx.reply(`welcome ${ctx.update.message.chat.username}`, {
+        ctx.reply(`welcome ${ctx.update.message.chat.username || ctx.update.message.chat.id}`, {
             reply_markup : {
                 keyboard : [[{text:"play", web_app: {url:`https://asikion-bot.netlify.app/start-app/${ctx.update.message.chat.username || ctx.update.message.chat.id},${ctx.startPayload}`}}]]
             }
