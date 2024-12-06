794fd0906670f5451a853a807f9e8780899aa776
ced007 <99514468+ced007@users.noreply.github.com>
2024-12-06 10:44:02 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,13 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
     bot.start((ctx) => {
         ctx.reply(`welcome ${ctx.update.message.chat.username || ctx.update.message.chat.id}`, {
             reply_markup : {
-                keyboard : [[{text:"play", web_app: {url:`https://ced-asiko-bot.netlify.app/start-app/${ctx?.update?.message?.chat?.username ?? ctx?.update?.message?.chat?.id},${ctx.startPayload}`}}]]
+                keyboard : [
+                    [
+                        {text:"play", web_app: {url:`https://ced-asiko-bot.netlify.app/start-app/${ctx?.update?.message?.chat?.username ?? ctx?.update?.message?.chat?.id},${ctx.startPayload}`}}
+                    ],
+                    resize_keyboard: true,
+                    one_time_keyboard: true,
+                ]
             }
         });
     })
