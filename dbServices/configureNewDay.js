f9a658608f32fe7848c1f0bf822e011785a967db
ced007 <99514468+ced007@users.noreply.github.com>
2024-12-11 08:04:46 +0100

    Update configureNewDay.js

--- dbServices/configureNewDay.js
+++ dbServices/configureNewDay.js
@@ -18,7 +18,7 @@ module.exports = async(request, response) =>{
             totalSpinsPerDay: 3,
             allowedDailySpins:15, 
             today: newDay,
-            isDaysReset: resData.presentDayIndex >= 7 || resData.isTodayClaimed === false ? true: false,
+            isDaysReset: resData.presentDayIndex > 6 || resData.isTodayClaimed === false ? true: false,
         };
 
         resData = await db.updateOne({ serverCookie: userToken}, resData);
